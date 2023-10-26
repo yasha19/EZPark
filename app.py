ebc58b66fe0793534f5d68eb4047ec5ba38d8348
Nabeel <nkhan12@uncc.edu>
2023-10-25 22:04:46 -0400

    Added connection to database

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
 
+db = mysql.connector.connect(
+    host='sql9.freesqldatabase.com',
+    user='sql9656653',
+    password='TiLR6twahS',
+    database='sql9656653'
+)
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM profile')
+    data = cursor.fetchall()
+    print(data)
+    cursor.close()
+    return render_template('index.html', data=data)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
