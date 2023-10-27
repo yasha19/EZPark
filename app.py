1defbbc7a833bfbca8d38d275712b3728f46bb36
Javan Thomas <jthom482@uncc.edu>
2023-10-26 22:08:01 -0400

    Updates folder structure and adds routes

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
+from python.session.session import Sessions
+from python.database.db import Database
 
 app = Flask(__name__)
+HOST, PORT = 'localhost', 8080
+global username, favorites, classes
+db = Database()
+sessions = Sessions()
 
-@app.route('/')
-def index():
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout_page():
+    return render_template('login.html', logged_out=True)
+
+@app.route('/home')
+def home_page():
     return render_template('home.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/add-classes')
+def add_classes_page():
+    return render_template('add_classes.html')
+
+@app.route('/add-classes', methods=['POST'])
+def add_classes_page():
+    return render_template('add_classes.html')
+
+@app.route('/add-favorites')
+def favorites_page():
+    return render_template('add_favorites.html')
+
+@app.route('/add-favorites', methods=['POST'])
+def favorites_page():
+    return render_template('add_favorites.html')
+
+@app.route('/alerts')
+def alerts_page():
+    return render_template('alerts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host=HOST, port=PORT)
