7022815607cdd20c95e78396d0771596a74454c1
Javan Thomas <jthom482@uncc.edu>
2023-10-27 04:20:44 -0400

    Adds functionality to back and logout buttons

--- app.py
+++ app.py
@@ -7,48 +7,50 @@ from python.database.db import Database
 app = Flask(__name__)
 HOST, PORT = 'localhost', 8080
 global username, favorites, classes, alerts
+global backDisplay
+backDisplay= True
 db = Database()
 sessions = Sessions()
 
 @app.route('/')
 def index_page():
-    return render_template('login.html')
+    return render_template('login.html', backDisplay=False)
 
 @app.route('/login')
 def login_page():
-    return render_template('login.html')
+    return render_template('login.html', backDisplay=False)
 
 @app.route('/logout')
 def logout_page():
-    return render_template('login.html')
+    return render_template('login.html', backDisplay=False)
 
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', backDisplay=False)
 
 @app.route('/map')
 def map_page():
-    return render_template('interactive_map.html')
+    return render_template('interactive_map.html', backDisplay=True)
 
 @app.route('/add-classes')
 def classes_page():
-    return render_template('add_classes.html')
+    return render_template('add_classes.html', backDisplay=True)
 
 @app.route('/add-classes', methods=['POST'])
 def add_classes_page():
-    return render_template('add_classes.html')
+    return render_template('add_classes.html', backDisplay=True)
 
 @app.route('/add-favorites')
 def favorites_page():
-    return render_template('add_favorites.html')
+    return render_template('add_favorites.html', backDisplay=True)
 
 @app.route('/add-favorites', methods=['POST'])
 def add_favorites_page():
-    return render_template('add_favorites.html')
+    return render_template('add_favorites.html', backDisplay=True)
 
 @app.route('/alerts')
 def alerts_page():
-    return render_template('alerts.html')
+    return render_template('alerts.html', backDisplay=True)
 
 if __name__ == '__main__':
     app.run(ssl_context='adhoc', debug=True, host=HOST, port=PORT)
