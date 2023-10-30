0082ae211aff0ade093f438acb372a7f5bae1033
Javan Thomas <jthom482@uncc.edu>
2023-10-29 20:15:05 -0400

    Adds favorites and classes templates

--- python/database/db.py
+++ python/database/db.py
@@ -11,31 +11,31 @@ class Database:
         )
         self.cursor = self.connection.cursor()
 
-    def insert_new_favorite(self, student_email: str) -> None:
+    def insert_new_favorite(self, student_id: int, parking_name:str) -> None:
         self.cursor.execute(
-            "INSERT INTO favorites () VALUES (?, ?, ?)", ())
+            "INSERT INTO favorites (favoritesID, fprofileID, parkingName) VALUES (?, ?, ?)", (None, student_id, parking_name))
         self.connection.commit()
 
-    def insert_new_class(self, student_email: str) -> None:
+    def insert_new_class(self, student_id: int, building_name: str, day: str, time: str) -> None:
         self.cursor.execute(
-            "INSERT INTO classes () VALUES (?, ?, ?)", ())
+            "INSERT INTO classes (classesID, cProfileID, buildingName, day, time) VALUES (?, ?, ?, ?)", (None, student_id, building_name, day, time))
         self.connection.commit()
 
     def get_all_parking_decks(self):
         self.cursor.execute("SELECT * FROM parking")
         return self.cursor.fetchall()
     
-    def get_preference_by_user(self, student_email: str):
-        self.cursor.execute("SELECT * FROM preferences where student_email = ?", student_email)
+    def get_profile_by_email(self, student_email: str):
+        self.cursor.execute("SELECT * FROM profile where email = ?", (student_email))
         return self.cursor.fetchall()
 
-    def get_all_classes_by_user(self, student_email: str):
-        self.cursor.execute("SELECT * FROM classes WHERE id = ?", student_email)
+    def get_all_classes_by_user(self, student_id: int):
+        self.cursor.execute("SELECT * FROM classes WHERE cProfileID= ?", (student_id))
         return self.cursor.fetchall()
 
-    def get_all_favorites_by_user(self, student_email: str):
+    def get_all_favorites_by_user(self, student_id: int):
         self.cursor.execute(
-            "SELECT * FROM favorites WHERE id = ?", student_email)
+            "SELECT * FROM favorites WHERE fprofileID = ?", (student_id))
         return self.cursor.fetchall()
 
     def get_alerts_by_date(self, daytime: dt.date):
