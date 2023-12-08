f76ed4c667120d8a63fba7cfab9c230fa5417a68
Johnson Busitu <johnsonbusitu2@gmail.com>
2023-12-07 21:28:59 -0500

    new changes

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-#!/usr/bin/env python3
-
 import datetime
+from datetime import datetime
+
+
+import os
+import base64
+from email.mime.text import MIMEText
+
+
 from google.oauth2 import id_token
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+
 from google.auth.transport import requests
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
 from flask import Flask, render_template, request, make_response, redirect, url_for, session
 from python.database.db import Database
 from python.web.parking import get_parking_availability
 from urllib.parse import parse_qs
+
 
 app = Flask(__name__)
 app.secret_key = 'uCraR5MZB/AvVo3Q24cBM/fZo5Kv/hV2HW9y0b3puClB25h0lbjBP6vYsHzz1hVY'
@@ -23,20 +36,29 @@ buildings = db.get_all_buildings()
 parking_decks = db.get_all_parking_decks()
 bus_locations = db.get_all_bus_locations()
 
+
+# Gmail API setup
+client_id = '1031925357556-g9tr3am18n1vg8ce88svenjgj82onrvt.apps.googleusercontent.com',
+client_secret = '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',
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+
+
+
 @app.route('/')
 def index_page():
     global userId
     if  isValidSession(userId):
         return redirect(url_for('home_page'))
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/login')
 def login_page():
     global userId
     if isValidSession(userId):
         return redirect(url_for('home_page'))
     return render_template('login.html', backDisplay=False, aboutDisplay=False, settingsDisplay=True, logDisplay=True)
-    
+   
 @app.route('/logout')
 def logout_page():
     global userId, profile, favorites, classes, alerts
@@ -47,13 +69,16 @@ def logout_page():
     alerts = None
     session.pop('user_id', None)
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/about')
 def about_page():
     global userId
     if isValidSession(userId):
         return render_template('about.html', backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
+
+
+
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile_page():
@@ -64,11 +89,17 @@ def profile_page():
         else:
             print("saving user settings")
 
+
+
+
             # profile.rec = request.form['rec']
             # profile.comType = request.form['comType']
             # db.update_profile(profile)
-            
+           
     return redirect(url_for('login_page'))
+
+
+
 
 @app.route('/home')
 def home_page():
@@ -79,41 +110,53 @@ def home_page():
             # profile = db.get_profile_by_id(userId)
         return render_template('home.html', profileData=profile, backDisplay=False, aboutDisplay=True)
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/home', methods=['POST'])
 def home_with_credentials_page():
     global userId, profile
-    
+   
     try:
         token = request.form['g_csrf_token']
         credential = request.form['credential']
         decoded_token = id_token.verify_oauth2_token(credential, requests.Request(), '1031925357556-g9tr3am18n1vg8ce88svenjgj82onrvt.apps.googleusercontent.com')
 
+
+
+
         # ID for student account to be used in all other calls
-        userid = decoded_token['sub'] 
+        userid = decoded_token['sub']
         userId = userid
         session['user_id'] = userid
         session['g_csrf_token'] = token
+
+
+
 
         # Get profile ID, insert new profile if userId doesn't exist
         profile = db.get_profile_by_id(userId)
         if profile == None or len(profile) == 0:
             # Email for student account to be used in all other calls
             email = decoded_token['email']
-            
+           
             # Student account name
             FName = decoded_token['given_name']
             LName = decoded_token['family_name']
-            
+           
             db.insert_new_profile(userId, FName, LName, email)
+
+
+
 
         home_page = make_response(redirect(url_for('home_page')))
         home_page.set_cookie('g_csrf_token', token)
 
+
+
+
         return home_page
     except ValueError:
         return redirect(url_for('login_page'))
-    
+   
 @app.route('/map')
 def map_page():
     global userId, parking_decks, classes, bus_locations
@@ -128,15 +171,21 @@ def map_page():
                     newClass = class_ + (address,)
                     courses.append(newClass)
 
+
+
+
         for lot in parking_decks:
             lotAddress = f'{lot[2]}, {lot[3]}, {lot[4]} {lot[5]}'
             newLot = (lot[1],) + (lotAddress,) + (lot[7],)
             lots.append(newLot)
         print(lots)
 
+
+
+
         return render_template('interactive_map.html', classData=courses, parkingData=lots, busData=bus_locations, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/classes', methods=['GET'])
 def classes_home_page():
     global userId, classes
@@ -144,7 +193,7 @@ def classes_home_page():
         classes = db.get_all_classes_by_user(userId)
         return render_template('classes.html', classData=classes, buildingData=buildings, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/classes', methods=['DELETE'])
 def delete_classes():
     global userId, classes
@@ -158,12 +207,18 @@ def delete_classes():
         return render_template('classes.html', classData=classes, buildingData=buildings, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
 
+
+
+
 @app.route('/add-classes', methods=['GET'])
 def add_classes_home_page():
     global userId, buildings
     if isValidSession(userId):
         return render_template('add_classes.html', buildingData=buildings, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
+
+
+
 
 @app.route('/add-classes', methods=['POST'])
 def add_classes():
@@ -177,6 +232,9 @@ def add_classes():
         return render_template('add_classes.html', buildingData=buildings, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
 
+
+
+
 @app.route('/favorites', methods=['GET'])
 def favorites_home_page():
     global userId, favorites
@@ -184,6 +242,9 @@ def favorites_home_page():
         favorites = db.get_all_favorites_by_user(userId)
         return render_template('favorites.html', favData=favorites, parkingData=parking_decks, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
+
+
+
 
 @app.route('/favorites', methods=['DELETE'])
 def delete_favorites():
@@ -198,12 +259,18 @@ def delete_favorites():
         return render_template('favorites.html', favData=favorites, parkingData=parking_decks, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
 
+
+
+
 @app.route('/add-favorites', methods=['GET'])
 def add_favorites_home_page():
     global userId, parking_decks
     if isValidSession(userId):
         return render_template('add_favorites.html', parkingData=parking_decks, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
+
+
+
 
 @app.route('/add-favorites', methods=['POST'])
 def add_favorites():
@@ -216,7 +283,7 @@ def add_favorites():
         db.insert_new_favorite(userId, location[0], capacity[0])
         return render_template('add_favorites.html', parkingData=parking_decks, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
-    
+   
 @app.route('/alerts')
 def alerts_page():
     print("alerts_page")
@@ -225,22 +292,86 @@ def alerts_page():
         now = datetime.datetime.now()
         # alerts = db.get_alerts_by_date(now)
 
+
+
+
         return render_template('alerts.html', alertData=alerts, backDisplay=True, aboutDisplay=False)
     return redirect(url_for('login_page'))
 
-@app.route('/feedback', methods=['GET','POST'])
+
+# @app.route('/feedback', methods=['GET','POST'])
+# def feedback_page():
+#     global userId, feedback, profile
+#     if isValidSession(userId):
+#         if request.method == 'GET':
+#             return render_template('feedback.html', profileData=profile, backDisplay=True, aboutDisplay=False)
+#         else:
+#             data = request.data.decode('utf-8')
+#             data = parse_qs(data)
+#             feedback = data['feedback']
+#             profile = db.get_profile_by_id(userId, feedback[0])
+#             return render_template('feedback.html', profileData=profile, backDisplay=True, aboutDisplay=False)
+#     return redirect(url_for('feedback_page'))
+
+
+def send_feedback_email(sender_email, receiver_email, subject, message_text):
+    creds = None
+
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json')
+
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+
+    service = build('gmail', 'v1', credentials=creds)
+
+
+    message = create_message(sender_email, receiver_email, subject, message_text)
+    sent_message = service.users().messages().send(userId='me', body=message).execute()
+    print('Message Id: %s' % sent_message['id'])
+
+
+def create_message(sender_email, receiver_email, subject, message_text):
+    message = MIMEText(message_text)
+    message['to'] = receiver_email
+    message['from'] = sender_email
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+
+@app.route('/feedback', methods=['GET', 'POST'])
 def feedback_page():
-    global userId, feedback, profile
+    global userId
+
+
     if isValidSession(userId):
         if request.method == 'GET':
-            return render_template('feedback.html', profileData=profile, backDisplay=True, aboutDisplay=False)
+            return render_template('feedback.html', backDisplay=True, aboutDisplay=False)
         else:
-            data = request.data.decode('utf-8')
-            data = parse_qs(data)
-            feedback = data['feedback']
-            profile = db.get_profile_by_id(userId, feedback[0])
-            return render_template('feedback.html', profileData=profile, backDisplay=True, aboutDisplay=False)
-    return redirect(url_for('feedback_page'))
+            sender_email = 'jbusitu@uncc.edu'  # will have to Change this to the email from which I'll send feedback
+            receiver_email = 'jbusitu@uncc.edu'  # will have to Change this to an email where feedback will be received
+            subject = 'Feedback from EZ Park App'
+            feedback_message = request.form.get('feedback', '')
+
+
+            send_feedback_email(sender_email, receiver_email, subject, feedback_message)
+
+
+            return render_template('feedback.html', backDisplay=True, aboutDisplay=False)
+    return redirect(url_for('login_page'))
+
+
 
 
 def isValidSession(user_id):
@@ -248,6 +379,7 @@ def isValidSession(user_id):
         print("User validated")
         return True
     return False
+
 
 
 
--- python/web/parking.py
+++ python/web/parking.py
@@ -1,8 +1,6 @@
-
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 def get_parking_availability():
     print('parking availability')
@@ -10,8 +8,7 @@ def get_parking_availability():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
 
-    path = ChromeDriverManager().install()
-    driver = webdriver.Chrome(executable_path=path, options=chrome_options)
+    driver = webdriver.Chrome("/chromedriver", options=chrome_options)
     url = "https://parkingavailability.charlotte.edu/"
     
     driver.get(url)
